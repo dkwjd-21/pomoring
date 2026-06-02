948c26ba0c3c6eebafb4ad08d882ebb760bb1823
dkwjd-21 <137870180+dkwjd-21@users.noreply.github.com>
2026-06-03 00:36:06 +0900

    chore: 실제 배포를 고려한 Java + React v4 모노레포 아키텍처 초기 세팅 완료

---
+++ pomoring-be/src/main/java/com/pomoring/be/PomoringBeApplication.java
@@ -0,0 +1,13 @@
+package com.pomoring.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomoringBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomoringBeApplication.class, args);
+    }
+
+}
---
+++ pomoring-be/src/test/java/com/pomoring/be/PomoringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomoring.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomoringBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
